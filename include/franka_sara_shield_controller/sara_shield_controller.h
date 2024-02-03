ddb5114bab1a65ddf6b25e3f94714bfeabf0f802
Manuel Vogel <ge87xig@mytum.de>
2024-02-03 11:51:42 +0000

    sara_shield can accept long term trajectories and control panda

--- include/franka_sara_shield_controller/sara_shield_controller.h
+++ include/franka_sara_shield_controller/sara_shield_controller.h
@@ -11,6 +11,7 @@
 #include <hardware_interface/robot_hw.h>
 #include <ros/node_handle.h>
 #include <ros/time.h>
+#include <std_msgs/Float32MultiArray.h>
 
 #include "safety_shield/safety_shield.h"
 
@@ -19,6 +20,7 @@ namespace franka_sara_shield_controller {
 class SaraShieldController : public controller_interface::MultiInterfaceController<
                                            hardware_interface::PositionJointInterface> {
  public:
+  // SaraShieldController();
   bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
   void starting(const ros::Time&) override;
   void update(const ros::Time&, const ros::Duration& period) override;
@@ -26,9 +28,20 @@ class SaraShieldController : public controller_interface::MultiInterfaceControll
  private:
   hardware_interface::PositionJointInterface* position_joint_interface_;
   std::vector<hardware_interface::JointHandle> position_joint_handles_;
-  ros::Duration elapsed_time_;
+  // ros::Duration elapsed_time_;
+
+  //todo remove and only use local vector
   std::array<double, 7> initial_pose_{};
   safety_shield::SafetyShield* shield_;
+
+  bool _new_goal;
+  std::vector<double> _goal_joint_pos;
+  std::vector<std::vector<double>> _human_meas;
+
+  ros::NodeHandle nh;
+  ros::Subscriber robot_goal_pos_sub_;
+
+  void goalJointPosCallback(const std_msgs::Float32MultiArray& msg);
 };
 
 }  // namespace franka_sara_shield_controller
--- src/sara_shield_controller.cpp
+++ src/sara_shield_controller.cpp
@@ -10,6 +10,8 @@
 
 namespace franka_sara_shield_controller {
 
+// SaraShieldController::SaraShieldController(){}
+
 bool SaraShieldController::init(hardware_interface::RobotHW* robot_hardware,
                                           ros::NodeHandle& node_handle) {
   position_joint_interface_ = robot_hardware->get<hardware_interface::PositionJointInterface>();
@@ -47,9 +49,9 @@ bool SaraShieldController::init(hardware_interface::RobotHW* robot_hardware,
       return false;
     }
   }
-  // safety shield
+  // safety shield values
   bool activate_shield = true;
-  double sample_time = 0.01;
+  double sample_time = 0.001;
   std::string folder = "/home/user/catkin_ws/src/franka_sara_shield_controller/config/";
   std::string trajectory_config_file = folder + "trajectory_parameters_panda.yaml";
   std::string robot_config_file = folder + "robot_parameters_panda.yaml";
@@ -62,6 +64,7 @@ bool SaraShieldController::init(hardware_interface::RobotHW* robot_hardware,
   double init_yaw = 0.0;
   std::vector<double> init_qpos = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
+  // safety shield init
   shield_ = new safety_shield::SafetyShield(
     sample_time,
     trajectory_config_file,
@@ -74,6 +77,16 @@ bool SaraShieldController::init(hardware_interface::RobotHW* robot_hardware,
     init_pitch,
     init_yaw,
     init_qpos);
+
+  // Dummy human measurement
+  _human_meas.resize(21);
+  for (int i=0; i<21; i++) {
+    _human_meas[i] = std::vector<double>{1200, 1200, 0};
+  }
+
+  // ros publishers and subsribers
+  robot_goal_pos_sub_ = nh.subscribe("/sara_shield/goal_joint_pos", 100, & SaraShieldController::goalJointPosCallback, this);
+
   return true;
 }
 
@@ -81,22 +94,57 @@ void SaraShieldController::starting(const ros::Time& /* time */) {
   for (size_t i = 0; i < 7; ++i) {
     initial_pose_[i] = position_joint_handles_[i].getPosition();
   }
-  elapsed_time_ = ros::Duration(0.0);
+  // elapsed_time_ = ros::Duration(0.0);
+
+  // reset sara shield
+  std::vector<double> initial_pose_vec(initial_pose_.begin(), initial_pose_.end());
+  shield_->reset(0, 0, 0, 0, 0, 0, initial_pose_vec, ros::Time::now().toSec());
 }
 
 void SaraShieldController::update(const ros::Time& /*time*/,
                                             const ros::Duration& period) {
-  elapsed_time_ += period;
+  // elapsed_time_ += period;
 
-  double delta_angle = M_PI / 16 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec())) * 0.2;
+  shield_->humanMeasurement(_human_meas, ros::Time::now().toSec());
+
+  // check if a new goal pose is set. If so, give a new LongTermTrajectory to sara shield
+  if(_new_goal){
+    _new_goal = false;
+    std::vector<double> qvel{0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    shield_->newLongTermTrajectory(_goal_joint_pos, qvel);
+    std::cout<<"new trajectory set"<<std::endl;
+  }
+
+  // Perform a sara shield update step
+  safety_shield::Motion next_motion = shield_->step(ros::Time::now().toSec());
+  //std::cout<<"time"<<ros::Time::now().toSec()<<std::endl;
+  std::vector<double> q = next_motion.getAngle(); 
+  //for(double qi:q){
+  //  std::cout<<qi<<" ";
+  //}
+  //std::cout<<std::endl;
+
+  if(!shield_->getSafety()){
+    ROS_ERROR("NOT SAFE");
+  }
+
+  // double delta_angle = M_PI / 16 * (1 - std::cos(M_PI / 5.0 * elapsed_time_.toSec())) * 0.2;
   for (size_t i = 0; i < 7; ++i) {
-    if (i == 4) {
-      position_joint_handles_[i].setCommand(initial_pose_[i] - delta_angle);
-    } else {
-      position_joint_handles_[i].setCommand(initial_pose_[i] + delta_angle);
-    }
+      position_joint_handles_[i].setCommand(q[i]);
   }
 }
+
+
+// ROS Callbacks
+
+void SaraShieldController::goalJointPosCallback(const std_msgs::Float32MultiArray& msg)
+{
+  std::vector<double> a(msg.data.begin(), msg.data.end());
+  _goal_joint_pos = a;
+  _new_goal = true;
+}
+
+
 
 }  // namespace franka_sara_shield_controller
 
