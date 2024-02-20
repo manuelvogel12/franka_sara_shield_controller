65ab3d8e91d405cc569fd71bd7f435707a3b591c
Jakob Thumm <jakob.thumm@web.de>
2024-02-20 15:48:35 -0800

    Added Cartesian impedance controller support

--- include/franka_sara_shield_controller/sara_shield_impedance_controller.h
+++ include/franka_sara_shield_controller/sara_shield_impedance_controller.h
@@ -1,28 +1,58 @@
-// Copyright (c) 2017 Franka Emika GmbH
-// Use of this source code is governed by the Apache-2.0 license, see LICENSE
+/* 
+This is an impedance controller that follows SaRA shield outputs.
+It has two modes:
+  - Joint impedance control to follow SaRA shield
+  - Cartesian impedance control to hold a static position
+
+Author: Jakob Thumm
+Date: 20.02.2024
+*/
 #pragma once
 
+// std
 #include <memory>
 #include <array>
 #include <string>
 #include <vector>
 #include <iterator>
+#include <mutex>
+#include <cmath>
+#include <algorithm>
 
+// Eigen
+#include <Eigen/Dense>
+
+// ROS
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/node_handle.h>
+#include <controller_interface/controller_base.h>
+#include <hardware_interface/hardware_interface.h>
 #include <controller_interface/multi_interface_controller.h>
+#include <dynamic_reconfigure/server.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/robot_hw.h>
 #include <realtime_tools/realtime_publisher.h>
-#include <ros/node_handle.h>
-#include <ros/time.h>
+#include <pluginlib/class_list_macros.h>
 
+// ROS messages
+#include <std_msgs/Bool.h>
 #include <std_msgs/Float32MultiArray.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <sensor_msgs/JointState.h>
+#include <geometry_msgs/PoseStamped.h>
 
+// Franka ROS
 #include <franka_example_controllers/JointTorqueComparison.h>
+#include <franka_example_controllers/compliance_paramConfig.h>
+#include <franka_example_controllers/pseudo_inversion.h>
 #include <franka_hw/franka_state_interface.h>
 #include <franka_hw/franka_model_interface.h>
 #include <franka_hw/trigger_rate.h>
+#include <franka/robot_state.h>
+#include <franka/rate_limiting.h>
+#include <franka/model.h>
+
 
 namespace franka_sara_shield_controller {
 
@@ -31,39 +61,178 @@ class SaraShieldImpedanceController : public controller_interface::MultiInterfac
                                            hardware_interface::EffortJointInterface,
                                            franka_hw::FrankaStateInterface> {
  public:
-  // SaraShieldController();
+  // <--------- PUBLIC Functions ----------->
   bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
-  void starting(const ros::Time&) override;
+  void starting(const ros::Time& time) override;
   void update(const ros::Time&, const ros::Duration& period) override;
 
  private:
-  
+  // Sends out the desired joint torque
   std::vector<hardware_interface::JointHandle> joint_handles_;
+  // Get the current robot state
   std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
+  // Get the current robot model
   std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
-  // ros::Duration elapsed_time_;
 
-  static constexpr double kDeltaTauMax{1.0};
+  /** 
+   * True if the controller is in the joint impedance mode.
+   * False if the controller is in the Cartesian impedance mode.
+  */
+  bool is_in_joint_impedance_state_ = false;
+
+  // Limits the change in torque. Currently not used.
+  static constexpr double k_delta_tau_max_{1.0};
+  // Factor for the joint velocity filter. 1 = measured vel, 0 = previous vel.
+  static constexpr double joint_vel_filter_{0.99};
+  // <------- Joint impedance controller parameters --------->
+
+  // Stiffness of the joint impedance controller
   std::vector<double> k_gains_;
+  // Damping of the joint impedance controller
   std::vector<double> d_gains_;
+  // Coriolis factor for the joint impedance controller. 1 = full, 0 = no coriolis force.
   double coriolis_factor_{1.0};
+
+  // <------- Cartesian impedance controller parameters --------->
+
+  /* This is used to slowly ramp up the current position/orientation/stiffness/damping 
+   * parameters of the joint the Cartesian controller to the desired parameters
+   * after changing the parameters.
+   */
+  double filter_params_{0.005};
+  /* Nullspace stiffness of the Cartesian controller.
+   * This defines the stiffness towards joint changes.
+   */ 
+  double nullspace_stiffness_{20.0};
+  double nullspace_stiffness_target_{20.0};
+  // Stiffness of the Cartesian impedance controller
+  Eigen::Matrix<double, 6, 6> cartesian_stiffness_;
+  Eigen::Matrix<double, 6, 6> cartesian_stiffness_target_;
+  // Damping of the Cartesian impedance controller
+  Eigen::Matrix<double, 6, 6> cartesian_damping_;
+  Eigen::Matrix<double, 6, 6> cartesian_damping_target_;
+  
+  // Desired EEF position
+  Eigen::Vector3d position_d_;
+  Eigen::Vector3d position_d_target_;
+  // Desired EEF orientation
+  Eigen::Quaterniond orientation_d_;
+  Eigen::Quaterniond orientation_d_target_;
+  // ???
+  std::mutex position_and_orientation_d_target_mutex_;
+  
+  
+  // Filtered joint velocity
   std::array<double, 7> dq_filtered_;
+  // Init robot pose
   std::array<double, 16> initial_pose_;
+  // Desired joint position
   std::vector<double> q_d_{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+  // Desired joint velocity.
   std::vector<double> dq_d_{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
+  // Trigger for outputting controller information (mainly for debugging purposes)
   franka_hw::TriggerRate rate_trigger_{1.0};
+  // Previous desired torque for calculating the torque error.
   std::array<double, 7> last_tau_d_{};
+  // Publisher that outputs the controller information (mainly for debugging purposes)
   realtime_tools::RealtimePublisher<franka_example_controllers::JointTorqueComparison> torques_publisher_;
 
+  // The node handle
   ros::NodeHandle nh;
+  // The node handle for dynamically setting the Cartesian impedance control parameters
+  ros::NodeHandle dynamic_reconfigure_compliance_param_node_;
+
+  // Dynamic reconfigure
+  std::unique_ptr<dynamic_reconfigure::Server<franka_example_controllers::compliance_paramConfig>>
+      dynamic_server_compliance_param_;
+
+  // <------- ROS Subscribers --------->
+  // Subscribes to the desired joint state that the robot should move to.
   ros::Subscriber desired_joint_state_sub_;
+  // Subscriber that changes the control mode from joint impedance (true) to Cartesian impedance (false)
+  ros::Subscriber impedance_mode_sub_;
+
+  // <------- ROS Publishers --------->
+  // Publishes the current joint position.
   ros::Publisher observed_joint_pos_pub_;
   
+  // <--------- PRIVATE Functions ----------->
+  /** Initialize the cartesian impedance controller.
+  */
+  void initializeCartesianImpedanceMode();
+
+  /** Calculate the joint torques when in joint impedance control mode.
+   * @param[in] joint_pos: current joint position
+   * @param[in] joint_vel: current joint velocity (often the filtered one)
+   * @param[in] coriolis: current coriolis force
+   * @returns desired joint torques
+   */
+  std::array<double, 7> calculateTorquesJointImpedanceControl(
+    const std::array<double, 7>& joint_pos,
+    const std::array<double, 7>& joint_vel,
+    const std::array<double, 7>& coriolis
+  );
+
+  /** Calculate the joint torques when in Cartesian impedance control mode.
+   * @param[in] joint_pos: current joint position
+   * @param[in] joint_vel: current joint velocity (often the filtered one)
+   * @param[in] joint_torque: current joint torque from robot state
+   * @param[in] O_T_EE: current transformation matrix from origin to end-effector
+   * @param[in] coriolis: current coriolis force
+   * @param[in] jacobian: current Jacobian matrix
+   * @returns desired joint torques
+   */
+  std::array<double, 7> calculateTorquesCartesianImpedanceControl(
+    std::array<double, 7>& joint_pos,
+    std::array<double, 7>& joint_vel,
+    std::array<double, 7>& joint_torque,
+    std::array<double, 16>& O_T_EE,
+    std::array<double, 7>& coriolis,
+    std::array<double, 42>& jacobian
+  );
+
+  // Brings the Cartesian impedance control parameters closer to their desired target. 
+  void rampUpImpedanceParameters();
+
+  /** Calculate the forward kinematics for the given joint configuration.
+   * @param[in] q: joint positions
+   * @returns 4x4 pose matrix for the given frame in base frame.
+   */
+  std::array<double, 16> forwardKinematics(const std::array<double, 7>& q);
+
   // callbacks
+  /** Set the desired joint position from the state.
+   * @param[in] msg: JointState message
+   */
   void desiredJointStateCallback(const sensor_msgs::JointState& msg);
 
-  // helper
+  /** Set the impedance controller mode.
+   * @details if msg=False, i.e., mode = Cartesian, we call initialize_cartesian_impedance_mode().
+   *
+   * @param[in] msg: Controller mode. 
+   *                 True = joint impedance controller. 
+   *                 False = Cartesian impedance controller.
+   */
+  void impedanceModeCallback(const std_msgs::Bool& msg);
+
+  /** Set the parameters of the Cartesian impedance controller.
+   * @param[in] config: Cartesian impedance controller parameters
+   * @param[in] level: ???
+   */
+  void complianceParamCallback(franka_example_controllers::compliance_paramConfig& config,
+                               uint32_t level);
+
+
+  // <--------- Currently unused functions ----------->
+  /** Calculate the torque saturation.
+   * @details We currently use franka::limitRate() instead of this function!
+   * @details This function makes sure the torque is not changing too fast.
+   *          Limits the change in torque to k_delta_tau_max_.
+   * @param[in] tau_d_calculated desired torque
+   * @param[in] tau_J_d current torque taken from the robot state
+   * @returns tau_d_saturated: saturated torque to execute.
+  */
   std::array<double, 7> saturateTorqueRate(
     const std::array<double, 7>& tau_d_calculated,
     const std::array<double, 7>& tau_J_d);  // NOLINT (readability-identifier-naming)
--- src/sara_shield_impedance_controller.cpp
+++ src/sara_shield_impedance_controller.cpp
@@ -1,21 +1,12 @@
 #include <franka_sara_shield_controller/sara_shield_impedance_controller.h>
 
-#include <cmath>
-
-#include <controller_interface/controller_base.h>
-#include <hardware_interface/hardware_interface.h>
-#include <hardware_interface/joint_command_interface.h>
-#include <pluginlib/class_list_macros.h>
-#include <ros/ros.h>
-#include <franka/robot_state.h>
-#include <franka/rate_limiting.h>
-
 namespace franka_sara_shield_controller {
 
 // SaraShieldImpedanceController::SaraShieldImpedanceController(){}
 
 bool SaraShieldImpedanceController::init(hardware_interface::RobotHW* robot_hardware,
                                           ros::NodeHandle& node_handle) {
+  // Get initial params joint impedance controller.
   std::string arm_id;
   if (!node_handle.getParam("arm_id", arm_id)) {
     ROS_ERROR("SaraShieldImpedanceController: Could not read parameter arm_id");
@@ -59,6 +50,36 @@ bool SaraShieldImpedanceController::init(hardware_interface::RobotHW* robot_hard
                     << coriolis_factor_);
   }
 
+  // Get initial params Cartesian impedance controller
+  double translational_stiffness;
+  if (!node_handle.getParam("translational_stiffness", translational_stiffness)) {
+    ROS_ERROR("SaraShieldImpedanceController: Could not read parameter translational_stiffness");
+    return false;
+  }
+  double rotational_stiffness;
+  if (!node_handle.getParam("rotational_stiffness", rotational_stiffness)) {
+    ROS_ERROR("SaraShieldImpedanceController: Could not read parameter rotational_stiffness");
+    return false;
+  }
+  cartesian_stiffness_target_.setIdentity();
+  cartesian_stiffness_target_.topLeftCorner(3, 3)
+      << translational_stiffness * Eigen::Matrix3d::Identity();
+  cartesian_stiffness_target_.bottomRightCorner(3, 3)
+      << rotational_stiffness * Eigen::Matrix3d::Identity();
+  cartesian_damping_target_.setIdentity();
+  // Damping ratio = 1
+  cartesian_damping_target_.topLeftCorner(3, 3)
+      << 2.0 * sqrt(translational_stiffness) * Eigen::Matrix3d::Identity();
+  cartesian_damping_target_.bottomRightCorner(3, 3)
+      << 2.0 * sqrt(rotational_stiffness) * Eigen::Matrix3d::Identity();
+
+  double nullspace_stiffness;
+  if (!node_handle.getParam("nullspace_stiffness", nullspace_stiffness)) {
+    ROS_ERROR("SaraShieldImpedanceController: Could not read parameter nullspace_stiffness");
+    return false;
+  }
+  nullspace_stiffness_target_ = nullspace_stiffness;
+
   // Get state interface
   auto* state_interface = robot_hardware->get<franka_hw::FrankaStateInterface>();
   if (state_interface == nullptr) {
@@ -93,6 +114,7 @@ bool SaraShieldImpedanceController::init(hardware_interface::RobotHW* robot_hard
     return false;
   }
 
+  // Get joint effort interface
   auto* effort_joint_interface = robot_hardware->get<hardware_interface::EffortJointInterface>();
   if (effort_joint_interface == nullptr) {
     ROS_ERROR_STREAM(
@@ -108,20 +130,62 @@ bool SaraShieldImpedanceController::init(hardware_interface::RobotHW* robot_hard
       return false;
     }
   }
-  torques_publisher_.init(node_handle, "torque_comparison", 1);
 
+  // Set init values to zero.
   std::fill(dq_filtered_.begin(), dq_filtered_.end(), 0);
+  position_d_.setZero();
+  orientation_d_.coeffs() << 0.0, 0.0, 0.0, 1.0;
+  position_d_target_.setZero();
+  orientation_d_target_.coeffs() << 0.0, 0.0, 0.0, 1.0;
 
+  // Init dynamic reconfigure ROS node
+  dynamic_reconfigure_compliance_param_node_ =
+      ros::NodeHandle("/sara_shield/dynamic_reconfigure_compliance_param_node");
+
+  dynamic_server_compliance_param_ = std::make_unique<
+      dynamic_reconfigure::Server<franka_example_controllers::compliance_paramConfig>>(
+        dynamic_reconfigure_compliance_param_node_);
+  dynamic_server_compliance_param_->setCallback(
+      boost::bind(&SaraShieldImpedanceController::complianceParamCallback, this, _1, _2));
+
+  // Init ROS subscribers
   desired_joint_state_sub_ = nh.subscribe("/sara_shield/desired_joint_state", 100, & SaraShieldImpedanceController::desiredJointStateCallback, this);
+  impedance_mode_sub_ = nh.subscribe("/sara_shield/set_impedance_mode", 100, & SaraShieldImpedanceController::impedanceModeCallback, this);
+
+  // Init ROS publisher
+  torques_publisher_.init(node_handle, "torque_comparison", 1);
   observed_joint_pos_pub_ = nh.advertise<std_msgs::Float32MultiArray>("/sara_shield/observed_joint_pos", 100);
 
   return true;
 }
 
 
-void SaraShieldImpedanceController::starting(const ros::Time& /* time */) {
+void SaraShieldImpedanceController::starting(const ros::Time& time) {
   franka::RobotState robot_state = state_handle_->getRobotState();
   q_d_.insert(q_d_.begin(), std::begin(robot_state.q), std::end(robot_state.q));
+  initializeCartesianImpedanceMode();
+}
+
+
+void SaraShieldImpedanceController::initializeCartesianImpedanceMode() {
+  // compute initial velocity with jacobian and set x_attractor and q_d_nullspace
+  // to initial configuration
+  franka::RobotState robot_state = state_handle_->getRobotState();
+  std::array<double, 7> q_d_arr;
+  std::copy_n(q_d_.begin(), 7, q_d_arr.begin());
+  std::array<double, 16> desired_transform_arr = model_handle_->getPose(
+      franka::Frame::kEndEffector,
+      q_d_arr,
+      robot_state.F_T_EE,
+      robot_state.EE_T_K);
+
+  Eigen::Affine3d desired_transform(Eigen::Matrix4d::Map(desired_transform_arr.data()));
+
+  // set equilibrium point to current state
+  position_d_ = desired_transform.translation();
+  orientation_d_ = Eigen::Quaterniond(desired_transform.rotation());
+  position_d_target_ = desired_transform.translation();
+  orientation_d_target_ = Eigen::Quaterniond(desired_transform.rotation());
 }
 
 
@@ -131,28 +195,45 @@ void SaraShieldImpedanceController::update(const ros::Time& /*time*/,
   std::array<double, 7> coriolis = model_handle_->getCoriolis();
   std::array<double, 7> gravity = model_handle_->getGravity();
 
-  double alpha = 0.99;
+  // Filter joint velocity
   for (size_t i = 0; i < 7; i++) {
-    dq_filtered_[i] = (1 - alpha) * dq_filtered_[i] + alpha * robot_state.dq[i];
+    dq_filtered_[i] = (1 - joint_vel_filter_) * dq_filtered_[i] + joint_vel_filter_ * robot_state.dq[i];
   }
 
+  // Calculate the joint torques
   std::array<double, 7> tau_d_calculated;
-  for (size_t i = 0; i < 7; ++i) {
-    tau_d_calculated[i] = coriolis_factor_ * coriolis[i] +
-                          k_gains_[i] * (q_d_[i] - robot_state.q[i]) +
-                          d_gains_[i] * (dq_d_[i] - dq_filtered_[i]);
+  if (is_in_joint_impedance_state_) {
+    tau_d_calculated = calculateTorquesJointImpedanceControl(robot_state.q, dq_filtered_, coriolis);
+  } else {
+    // get jacobian
+    std::array<double, 42> jacobian =
+        model_handle_->getZeroJacobian(franka::Frame::kEndEffector);
+    tau_d_calculated = calculateTorquesCartesianImpedanceControl(
+      robot_state.q,
+      dq_filtered_,
+      robot_state.tau_J_d,
+      robot_state.O_T_EE,
+      coriolis,
+      jacobian
+    );
   }
 
+  // Limit the joint difference
   // Maximum torque difference with a sampling rate of 1 kHz. The maximum torque rate is
   // 1000 * (1 / sampling_time).
   // std::array<double, 7> tau_d_saturated = saturateTorqueRate(tau_d_calculated, robot_state.tau_J_d);
   std::array<double, 7> tau_d_rate_limited =
           franka::limitRate(franka::kMaxTorqueRate, tau_d_calculated, robot_state.tau_J_d);
 
+  // Set the joint torque command
   for (size_t i = 0; i < 7; ++i) {
     joint_handles_[i].setCommand(tau_d_rate_limited[i]);
   }
 
+  // Ramp up Impedance controller parameters to desired value.
+  rampUpImpedanceParameters();
+
+  // Publish the torque outputs for debugging
   if (rate_trigger_() && torques_publisher_.trylock()) {
     std::array<double, 7> tau_j = robot_state.tau_J;
     std::array<double, 7> tau_error;
@@ -174,17 +255,138 @@ void SaraShieldImpedanceController::update(const ros::Time& /*time*/,
     last_tau_d_[i] = tau_d_calculated[i] + gravity[i];
   }
 
-  // observe joint pose
+  // Publish the observed joint position
   std_msgs::Float32MultiArray pos_msg;
   std::vector<float> observed_pose_float(robot_state.q.begin(), robot_state.q.end());
   pos_msg.data = observed_pose_float;
   observed_joint_pos_pub_.publish(pos_msg);
 }
 
+
+std::array<double, 7> SaraShieldImpedanceController::calculateTorquesJointImpedanceControl(
+    const std::array<double, 7>& joint_pos,
+    const std::array<double, 7>& joint_vel,
+    const std::array<double, 7>& coriolis
+  ) {
+  std::array<double, 7> tau_d_calculated;
+  for (size_t i = 0; i < 7; ++i) {
+    tau_d_calculated[i] = coriolis_factor_ * coriolis[i] +
+                          k_gains_[i] * (q_d_[i] - joint_pos[i]) +
+                          d_gains_[i] * (dq_d_[i] - joint_vel[i]);
+  }
+  return tau_d_calculated;
+}
+
+
+std::array<double, 7> SaraShieldImpedanceController::calculateTorquesCartesianImpedanceControl(
+    std::array<double, 7>& joint_pos,
+    std::array<double, 7>& joint_vel,
+    std::array<double, 7>& joint_torque,
+    std::array<double, 16>& O_T_EE,
+    std::array<double, 7>& coriolis,
+    std::array<double, 42>& jacobian
+  ) {
+  // convert to Eigen
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> coriolis_eigen(coriolis.data());
+  Eigen::Map<Eigen::Matrix<double, 6, 7>> jacobian_eigen(jacobian.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> q_eigen(joint_pos.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> q_d_eigen(q_d_.data(), 7);
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> dq_eigen(joint_vel.data());
+  Eigen::Map<Eigen::Matrix<double, 7, 1>> tau_J_d_eigen(joint_torque.data());
+  Eigen::Affine3d transform(Eigen::Matrix4d::Map(O_T_EE.data()));
+  Eigen::Vector3d position(transform.translation());
+  Eigen::Quaterniond orientation(transform.rotation());
+  // compute error to desired pose
+  // position error
+  Eigen::Matrix<double, 6, 1> error;
+  error.head(3) << position - position_d_;
+
+  // orientation error
+  if (orientation_d_.coeffs().dot(orientation.coeffs()) < 0.0) {
+    orientation.coeffs() << -orientation.coeffs();
+  }
+  // "difference" quaternion
+  Eigen::Quaterniond error_quaternion(orientation.inverse() * orientation_d_);
+  error.tail(3) << error_quaternion.x(), error_quaternion.y(), error_quaternion.z();
+  // Transform to base frame
+  error.tail(3) << -transform.rotation() * error.tail(3);
+
+  // compute control
+  // allocate variables
+  Eigen::VectorXd tau_task(7), tau_nullspace(7), tau_d(7);
+
+  // pseudoinverse for nullspace handling
+  // kinematic pseuoinverse
+  Eigen::MatrixXd jacobian_transpose_pinv;
+  franka_example_controllers::pseudoInverse(jacobian_eigen.transpose(), jacobian_transpose_pinv);
+
+  // Cartesian PD control with damping ratio = 1
+  tau_task << jacobian_eigen.transpose() *
+              (-cartesian_stiffness_ * error - cartesian_damping_ * (jacobian_eigen * dq_eigen));
+  // nullspace PD control with damping ratio = 1
+  tau_nullspace << (Eigen::MatrixXd::Identity(7, 7) -
+                    jacobian_eigen.transpose() * jacobian_transpose_pinv) *
+                       (nullspace_stiffness_ * (q_d_eigen - q_eigen) -
+                        (2.0 * sqrt(nullspace_stiffness_)) * dq_eigen);
+  // Desired torque
+  tau_d << tau_task + tau_nullspace + coriolis_factor_ * coriolis_eigen;
+  std::array<double, 7> final_torque;
+  std::copy_n(tau_d.data(), 7, final_torque.begin());
+  return final_torque;
+}
+
+
+void SaraShieldImpedanceController::rampUpImpedanceParameters() {
+  cartesian_stiffness_ =
+      filter_params_ * cartesian_stiffness_target_ + (1.0 - filter_params_) * cartesian_stiffness_;
+  cartesian_damping_ =
+      filter_params_ * cartesian_damping_target_ + (1.0 - filter_params_) * cartesian_damping_;
+  nullspace_stiffness_ =
+      filter_params_ * nullspace_stiffness_target_ + (1.0 - filter_params_) * nullspace_stiffness_;
+  std::lock_guard<std::mutex> position_d_target_mutex_lock(
+      position_and_orientation_d_target_mutex_);
+  position_d_ = filter_params_ * position_d_target_ + (1.0 - filter_params_) * position_d_;
+  orientation_d_ = orientation_d_.slerp(filter_params_, orientation_d_target_);
+}
+
+
 void SaraShieldImpedanceController::desiredJointStateCallback(const sensor_msgs::JointState& msg){
   q_d_ = msg.position;
   dq_d_ = msg.velocity;
+  if (is_in_joint_impedance_state_ == false) {
+    initializeCartesianImpedanceMode();
+  }
 }
+
+
+void SaraShieldImpedanceController::impedanceModeCallback(const std_msgs::Bool& msg) {
+  if (is_in_joint_impedance_state_ == msg.data) {
+    return;
+  }
+  is_in_joint_impedance_state_ = msg.data;
+  if (is_in_joint_impedance_state_ == false) {
+    initializeCartesianImpedanceMode();
+  }
+}
+
+
+void SaraShieldImpedanceController::complianceParamCallback(
+    franka_example_controllers::compliance_paramConfig& config,
+    uint32_t /*level*/) {
+  cartesian_stiffness_target_.setIdentity();
+  cartesian_stiffness_target_.topLeftCorner(3, 3)
+      << config.translational_stiffness * Eigen::Matrix3d::Identity();
+  cartesian_stiffness_target_.bottomRightCorner(3, 3)
+      << config.rotational_stiffness * Eigen::Matrix3d::Identity();
+  cartesian_damping_target_.setIdentity();
+  // Damping ratio = 1
+  cartesian_damping_target_.topLeftCorner(3, 3)
+      << 2.0 * sqrt(config.translational_stiffness) * Eigen::Matrix3d::Identity();
+  cartesian_damping_target_.bottomRightCorner(3, 3)
+      << 2.0 * sqrt(config.rotational_stiffness) * Eigen::Matrix3d::Identity();
+  nullspace_stiffness_target_ = config.nullspace_stiffness;
+}
+
 
 std::array<double, 7> SaraShieldImpedanceController::saturateTorqueRate(
     const std::array<double, 7>& tau_d_calculated,
@@ -192,7 +394,7 @@ std::array<double, 7> SaraShieldImpedanceController::saturateTorqueRate(
   std::array<double, 7> tau_d_saturated{};
   for (size_t i = 0; i < 7; i++) {
     double difference = tau_d_calculated[i] - tau_J_d[i];
-    tau_d_saturated[i] = tau_J_d[i] + std::max(std::min(difference, kDeltaTauMax), -kDeltaTauMax);
+    tau_d_saturated[i] = tau_J_d[i] + std::max(std::min(difference, k_delta_tau_max_), -k_delta_tau_max_);
   }
   return tau_d_saturated;
 }
